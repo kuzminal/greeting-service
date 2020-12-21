edf64c4c89799b8918ea733c4dded23780ed253a
kuzminal <kuzmin.al35@gmail.com>
2020-12-21 22:03:41 +0300

    Added properties as a bean with spring-boot-configuration-processor

--- src/main/java/com/kuzmin/greetingservice/GreetingServiceApplication.java
+++ src/main/java/com/kuzmin/greetingservice/GreetingServiceApplication.java
@@ -2,8 +2,10 @@ package com.kuzmin.greetingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class GreetingServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/kuzmin/greetingservice/config/GreetingProperties.java
@@ -0,0 +1,18 @@
+package com.kuzmin.greetingservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "greetings")
+public class GreetingProperties {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
